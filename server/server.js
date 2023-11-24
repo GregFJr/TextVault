c22df98fb25fe8d718734b99eee1abb079e397cd
GregFJr <finney101690@gmail.com>
2023-11-24 13:16:59 -0700

    installed dependencies and setup webpack.config.js

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the static files from the React app
+
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-// Handles any requests that don't match the ones above
+
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/dist/index.html'));
 });
