eb2e3fbb72eb0587fa9d7806e1c8552ca7fd4ed5
GregFJr <finney101690@gmail.com>
2023-11-24 13:14:42 -0700

    some initial folder setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
